993aa49267bb009aa3f2827dbe4dc19ceaa91599
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-05 12:24:45 +0600

    create navbar & home components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import './App.css'
+import Navbar from "./Components/Navbar.tsx";
+import {Route, Routes} from "react-router-dom";
+import Home from "./Components/Home.tsx";
 
 function App() {
 
   return (
     <>
+      <Navbar/>
+      <div>
+        <Routes>
+          <Route path="/" element={(<Home />)}></Route>
+        </Routes>
+      </div>
     </>
   )
 }
