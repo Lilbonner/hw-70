82546b5c354cbc3c1bdbf9555871cca7eecd7c8b
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-05 13:48:59 +0600

    create form component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css'
 import Navbar from "./Components/Navbar.tsx";
 import {Route, Routes} from "react-router-dom";
 import Home from "./Components/Home.tsx";
+import Form from "./Components/Form.tsx";
 
 function App() {
 
@@ -10,7 +11,8 @@ function App() {
       <Navbar/>
       <div>
         <Routes>
-          <Route path="/" element={(<Home />)}></Route>
+            <Route path="/" element={(<Home />)}/>
+            <Route path="Form" element={<Form/>}/>
         </Routes>
       </div>
     </>
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Navbar: React.FC = () => {
     return (
         <div className="flex bg-emerald-100 h-14 justify-between px-4 items-center">
             <h2 className="text-3xl ml-24 mt-1">Contacts</h2>
-            <button className="text-2xl mr-11 border-2 border-black bg-gray-200 w-60 ">Add new contacts</button>
+            <button className="text-2xl mr-11 border-2 border-black bg-gray-200 w-60 ">
+            <Link to="/Form">Add new contacts</Link>
+            </button>
         </div>
     );
 };
