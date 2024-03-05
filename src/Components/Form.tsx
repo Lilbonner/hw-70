5c37fada4562f42978d62544d46e81a436ee1d25
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-05 13:49:36 +0600

    added stylization to form component

--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -1,9 +1,26 @@
+import React from 'react';
+import {Link} from "react-router-dom";
 
-
-const Form = () => {
+const Form: React.FC = () => {
     return (
-        <div>
-
+        <div className="w-80 h-fit ml-10 mt-10 block border-2 border-black ">
+            <h2 className="text-4xl mb-10">Add/Edit contact</h2>
+            <div className="mb-4">
+                <label className="">Name: <input className="border-2 border-black rounded-md ml-2"/></label>
+            </div>
+            <div className="mb-4">
+            <label>Phone: <input className="border-2 border-black rounded-md ml-1"/></label>
+            </div>
+            <div className="mb-4">
+            <label>Email: <input className="border-2 border-black rounded-md ml-3"/></label>
+            </div>
+            <div className="mb-4">
+            <label>Photo: <input className="border-2 border-black rounded-md ml-2"/></label>
+            </div>
+            <div className="mt-14">
+                <button className="w-14 bg-gray-300 ml-14">save</button>
+                <button className="w-40 bg-gray-300 ml-6"><Link to="/">back to contacts</Link></button>
+            </div>
         </div>
     );
 };
