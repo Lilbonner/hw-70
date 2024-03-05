e61d2b8f7c471b542a72156761269380915f438d
Daniyar Nasirov <lilbonnie666@gmail.com>
2024-03-05 12:25:27 +0600

    added stylization to navbar

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
     return (
-        <div>
-            
+        <div className="flex bg-emerald-100 h-14 justify-between px-4 items-center">
+            <h2 className="text-3xl ml-24 mt-1">Contacts</h2>
+            <button className="text-2xl mr-11 border-2 border-black bg-gray-200 w-60 ">Add new contacts</button>
         </div>
     );
 };
